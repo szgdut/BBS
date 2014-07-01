a2fa06b16a680690bd3e4b36b9b39a3b98d02d16
leonshao <shao830417@163.com>
2014-07-02 01:02:17 +0800

    Author: leonshao Description: 首页界面增加导航"投票", 并设置权限.

---
+++ data/sysdata/cache_adminextend.php
@@ -0,0 +1,8 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 960e4c742e8c79d812ff604393c47a0a
+
+$adminextend = array (
+  0 => 'cloud.php',
+);
+?>
---
+++ data/sysdata/cache_mobile.php
@@ -0,0 +1,6 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 4a475bd8a41e4ffa8207f85b0612d94a
+
+$mobilecheck = '{"discuzversion":"X3.1","charset":"utf-8","version":"3","pluginversion":"1.3.1","oemversion":"0","regname":"register","qqconnect":"0","sitename":"Discuz! Board","mysiteid":"","ucenterurl":"http:\\/\\/bbs.szgdut.net\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
+?>
