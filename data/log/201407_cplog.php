98b4cecc641ea10b72f93dbed532b2437bc4ee33
leonshao <shao830417@163.com>
2014-07-04 00:48:32 +0800

    Author: leonshao Description: 补齐提交

---
+++ data/log/201407_cplog.php
@@ -0,0 +1,3 @@
+<?PHP exit;?>	1404207915	admin	1	127.0.0.1		GET={}; POST={};
+<?PHP exit;?>	1404207917	admin	1	127.0.0.1	index	GET={}; POST={};
+<?PHP exit;?>	1404207983	admin	1	127.0.0.1	nav	GET={}; POST={};
